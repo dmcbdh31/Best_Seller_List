2e92dcb12c43a295ffa3f24a687987857eeb08d5
dmcbdh31 <dmcbdh@gmail.com>
2022-11-14 12:07:31 -0500

    First draft for the best sellers list project.

---
+++ book.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self) -> None:
+        self.name = "Name"
+        self.author = "Author"
+        self.user_rating = "User Rating"
+        self.number_of_reviews = "Reviews"
+        self.price = "Price"
+        self.year = "Year"
+        self.genre = "Genre"
+
+def book_name(self):
+    self.name = input('Enter Book Name:     ')
+
+def book_author(self):
+    self.author = input('Enter Author:      ')
+
+def book_user_rating(self):
+    self.user_rating = input('Enter User Rating:    ')
+
+def book_number_of_reviews(self):
+    self.number_of_reviews = input('Enter Reviews:  ')
+
+def book_price(self):
+    self.price = input('Enter Price:    ')
+
+def book_year(self):
+    self.year = input('Enter Year:  ')
+
+def book_genre(self):
+    self.genre = input('Enter Genre:    ')
